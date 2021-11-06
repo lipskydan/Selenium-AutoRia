f28e0259bcdbb1106b1f09e7d5aee06eaedb6875
danial <lipsky@knu.ua>
2021-11-06 18:57:02 +0200

    added some test-cases

--- app/src/test/java/com/example/app/AutoRiaMainPage.java
+++ app/src/test/java/com/example/app/AutoRiaMainPage.java
@@ -4,11 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.concurrent.TimeUnit;
 
 public class AutoRiaMainPage {
-    WebDriver driver;
+    public WebDriver driver;
     String baseUrl;
 
     public AutoRiaMainPage(){
@@ -20,18 +21,57 @@ public class AutoRiaMainPage {
         driver.get(baseUrl);
     }
 
-    public void openListOfRegion(){
-        WebElement element = driver.findElement(By.xpath("//label[@for='brandTooltipBrandAutocompleteInput-region' and  @class='m-label']"));
+    public void openListOfRegionForAllOrBuOrOrder(){
+        WebElement element = driver.findElement(By.id("brandTooltipBrandAutocomplete-region"));
         element.click();
     }
 
+    public void selectOfRegionForNew(String regionName){
+        WebElement element = driver.findElement(By.id("regionCenters"));
+        Select sel = new Select(element);
+        sel.selectByVisibleText(regionName);
+    }
+
     public void chooseRegionByName(String regionName){
-        WebElement element = driver.findElement(By.linkText(regionName));
+        WebElement element = driver.findElement(By.xpath("//ul[@class=\"unstyle scrollbar autocomplete-select\"]//li//a[contains(text(),'" + regionName + "')]"));
         element.click();
     }
 
     public WebElement getSearchButton(){
         return driver.findElement(By.className("button"));
     }
+
+    public void setNewCars(){
+        WebElement element = driver.findElement(By.xpath("//label[@for='naRadioType']"));
+        element.click();
+    }
+
+    public void setOrderCars(){
+        WebElement element = driver.findElement(By.xpath("//label[@for='orderRadioType']"));
+        element.click();
+    }
+
+    public void setBuCars(){
+        WebElement element = driver.findElement(By.xpath("//label[@for='buRadioType']"));
+        element.click();
+    }
+
+    public void setAllCars(){
+        WebElement element = driver.findElement(By.xpath("//label[@for='allRadioType']"));
+        element.click();
+    }
+
+    public void selectYearFrom(String year){
+        WebElement element = driver.findElement(By.id("yearFrom"));
+        Select sel = new Select(element);
+        sel.selectByValue(year);
+    }
+
+    public void selectYearTo(String year){
+        WebElement element = driver.findElement(By.id("yearTo"));
+        Select sel = new Select(element);
+        sel.selectByValue(year);
+    }
+
 
 }
--- app/src/test/java/com/example/app/AutoRiaMainPageTest.java
+++ app/src/test/java/com/example/app/AutoRiaMainPageTest.java
@@ -1,28 +1,104 @@
 package com.example.app;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.WebElement;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
 
 
 public class AutoRiaMainPageTest {
-    AutoRiaMainPage mainPage = new AutoRiaMainPage();
+    AutoRiaMainPage mainPage;
 
-    @Test
-    public void testFindAnyCarFromKiev() {
-       mainPage.openListOfRegion();
-       mainPage.chooseRegionByName("Київ");
-       WebElement searchButton = mainPage.getSearchButton();
-       searchButton.click();
+    @Before
+    public void setUp(){
+       mainPage = new AutoRiaMainPage();
     }
 
+
+    @Test
+    public void testFindAnyCarFromZhytomyr() throws InterruptedException {
+        mainPage.setAllCars();
+        Thread.sleep(2000);
+        mainPage.openListOfRegionForAllOrBuOrOrder();
+        mainPage.chooseRegionByName("Житомир");
+        Thread.sleep(2000);
+        WebElement searchButton = mainPage.getSearchButton();
+        searchButton.click();
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void testFindUsedCarFromKiev() throws InterruptedException {
+        mainPage.setBuCars();
+        Thread.sleep(2000);
+        mainPage.openListOfRegionForAllOrBuOrOrder();
+        mainPage.chooseRegionByName("Київ");
+        Thread.sleep(2000);
+        WebElement searchButton = mainPage.getSearchButton();
+        searchButton.click();
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void testFindNewCarFromDonetskRegion() throws InterruptedException {
+        mainPage.setNewCars();
+        Thread.sleep(2000);
+        mainPage.selectOfRegionForNew("Донецька обл.");
+        Thread.sleep(2000);
+        WebElement searchButton = mainPage.getSearchButton();
+        searchButton.click();
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void testFindOrderCarFromTernopil() throws InterruptedException {
+        mainPage.setOrderCars();
+        Thread.sleep(2000);
+        mainPage.openListOfRegionForAllOrBuOrOrder();
+        mainPage.chooseRegionByName("Тернопіль");
+        Thread.sleep(2000);
+        WebElement searchButton = mainPage.getSearchButton();
+        searchButton.click();
+        Thread.sleep(5000);
+    }
+
+
+    @Test
+    public void testFindAnyCarFrom2010Year() throws InterruptedException {
+        mainPage.setAllCars();
+        Thread.sleep(2000);
+        mainPage.selectYearFrom("2010");
+        Thread.sleep(2000);
+        WebElement searchButton = mainPage.getSearchButton();
+        searchButton.click();
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void testFindAnyCarTo2018Year() throws InterruptedException {
+        mainPage.setAllCars();
+        Thread.sleep(2000);
+        mainPage.selectYearTo("2018");
+        Thread.sleep(2000);
+        WebElement searchButton = mainPage.getSearchButton();
+        searchButton.click();
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void testFindAnyCarFrom2008YearTo2013Year() throws InterruptedException {
+        mainPage.setAllCars();
+        Thread.sleep(2000);
+        mainPage.selectYearFrom("2008");
+        Thread.sleep(2000);
+        mainPage.selectYearTo("2013");
+        WebElement searchButton = mainPage.getSearchButton();
+        searchButton.click();
+        Thread.sleep(2000);
+    }
+
+    @After
+    public void tearDown(){
+        mainPage.driver.quit();
+    }
 }
